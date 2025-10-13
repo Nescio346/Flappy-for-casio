0b5d6514ee1241d8a745b913d8414b511b3b4850
Nescio346 <michaschenk16@gmail.com>
2025-10-13 11:47:01 +0200

    Add files via upload

--- Flappy/build/Flappy.d
+++ Flappy/build/Flappy.d
@@ -1,6 +1,9 @@
 Flappy.o: C\:/PrizmSDK/projects/Flappy/src/Flappy.c \
  C\:/PrizmSDK/include/fxcg/display.h C\:/PrizmSDK/include/fxcg/keyboard.h \
- C\:/PrizmSDK/include/fxcg/system.h
+ C\:/PrizmSDK/include/fxcg/system.h C\:/PrizmSDK/include/stdlib.h \
+ C\:/PrizmSDK/include/stddef.h
 C\:/PrizmSDK/include/fxcg/display.h:
 C\:/PrizmSDK/include/fxcg/keyboard.h:
 C\:/PrizmSDK/include/fxcg/system.h:
+C\:/PrizmSDK/include/stdlib.h:
+C\:/PrizmSDK/include/stddef.h:
--- Flappy/src/Flappy.c
+++ Flappy/src/Flappy.c
@@ -1,28 +1,42 @@
 #include <fxcg/display.h>
 #include <fxcg/keyboard.h>
 #include <fxcg/system.h>
+#include <stdlib.h>
 
 #define SCREEN_W 383
 #define SCREEN_H 216
 #define BLOCK 25
 #define JUMP_POWER 4
 #define GRAVITY 2
+#define PILLAR_W 10
+#define PILLAR_H 85
 
 // Prototypes
 int jump(int *y, int *springmulti);
-void draw(int *x, int *y);
+void draw(int *x, int *y, int *px, int *py);
 int gravity(int *y, int *valtijd);
 
 int main(void) {
     int x = 50;
     int y = 0;
+    int px = 380;
+    int py = 50;
     int last = 1;
 	int valtijd = 0;
 	int springtijd = 0;
 	int springmulti = 4;
+	int time = 0;
 
     while (1) {
         CMT_Delay_100micros(200);
+		time++;
+
+		if (time %= 5) px -= 6;
+
+		if (px <= 0 + PILLAR_W) {
+			px = 382;
+			py = rand() % (SCREEN_H - PILLAR_H);
+		}
 
 		if (springtijd == 0) {
 			valtijd++;
@@ -46,7 +60,7 @@ int main(void) {
         else if (PRGM_GetKey() != 28) last = 1;
         if (PRGM_GetKey() == 47) break;
 
-        draw(&x, &y);
+        draw(&x, &y, &px, &py);
     }
 
     return 0;
@@ -58,11 +72,16 @@ int gravity(int *y, int *valtijd) {
     return *y;
 }
 
-void draw(int *x, int *y) {
+void draw(int *x, int *y, int *px, int *py) {
     Bdisp_AllClr_VRAM();
+	for (int ipx = 0; ipx < PILLAR_W; ipx++) {
+		for (int ipy = 0; ipy < PILLAR_H; ipy++) {
+			Bdisp_SetPoint_VRAM(*px + ipx, *py + ipy, 0x0FF0);
+		}
+	}
     for (int ix = 0; ix <= BLOCK; ix++) {
         for (int iy = 0; iy <= BLOCK; iy++) {
-            Bdisp_SetPoint_VRAM((*x) + ix, (*y) + iy, 0xF800); // red
+            Bdisp_SetPoint_VRAM(*x + ix, *y + iy, 0xF800); // red
         }
     }
     Bdisp_PutDisp_DD();
